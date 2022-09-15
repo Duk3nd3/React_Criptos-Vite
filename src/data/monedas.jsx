c6d49be21201495ab2c0bb4ba33e5651aa43f662
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-09-15 15:24:04 -0300

    Reemplazo de moneda Argentina por moneda Brasilera

--- src/data/monedas.jsx
+++ src/data/monedas.jsx
@@ -1,7 +1,7 @@
 //Arreglo estatico para el Select
 const monedas = [
 	{ id: 'USD', nombre: 'Dolar de Estados Unidos' },
-	{ id: 'ARG', nombre: 'Peso de Argentina' },
+	{ id: 'BRL', nombre: 'Real de Brasil' },
 	{ id: 'EUR', nombre: 'Euro' },
 	{ id: 'GBP', nombre: 'Libra Esterlina' },
 ];
